f273a351cd98dcbf4a08f861fda8f2393b8f28c1
Paula Pereira <pspereira_@hotmail.com>
2018-12-22 13:42:32 +0000

    refactoring da classe Administrador de eventos

--- src/Business/AdministradorDeEventos.java
+++ src/Business/AdministradorDeEventos.java
@@ -4,7 +4,6 @@ package Business;
  * Created by luismp on 11/11/2018.
  */
 public class AdministradorDeEventos extends Utilizador {
-    private boolean estado;
 
     /*
     * Constutores
@@ -12,29 +11,10 @@ public class AdministradorDeEventos extends Utilizador {
 
     public AdministradorDeEventos(){
         super();
-        this.estado = true;
     }
 
-    public AdministradorDeEventos(int idUtilizador, String email, String password, String nome, boolean estado){
-        super(idUtilizador,email,password,nome);
-        this.estado=estado;
-    }
-
-    public AdministradorDeEventos(AdministradorDeEventos a){
-        super(a.getIdUtilizador(),a.getEmail(),a.getPassword(),a.getNome());
-        this.setEstado(a.getEstado());
-    }
-
-    /*
-    * Getters e Setters
-    * */
-
-    public void setEstado(boolean estado) {
-        this.estado = estado;
-    }
-
-    public boolean getEstado(){
-        return this.estado;
+    public AdministradorDeEventos(AdministradorDeEventos admin){
+        super(admin.getIdUtilizador(),admin.getEmail(),admin.getPassword(),admin.getNome());
     }
 
     /*
@@ -52,19 +32,21 @@ public class AdministradorDeEventos extends Utilizador {
 
         AdministradorDeEventos admin = (AdministradorDeEventos) object;
 
-        if(this.getIdUtilizador() == admin.getIdUtilizador()) return true ;
-        else return false;
+        return (this.getIdUtilizador() == admin.getIdUtilizador());
     }
 
     @Override
     public String toString(){
-        StringBuilder s = new StringBuilder();
-        s.append("----Administrador----\n");
-        s.append("ID : " + this.getIdUtilizador() + "\n");
-        s.append("Email : " + this.getEmail() + "\n");
-        s.append("Nome : " + this.getNome() + "\n");
-        s.append("------------------\n");
-        return s.toString();
+        StringBuilder string = new StringBuilder();
+        string
+            .append("----Administrador----\nID : ")
+            .append(this.getIdUtilizador())
+            .append("\nEmail : ")
+            .append(this.getEmail())
+            .append("\nNome : ")
+            .append( this.getNome() )
+            .append("\n------------------\n");
+        return string.toString();
     }
 
     /*
